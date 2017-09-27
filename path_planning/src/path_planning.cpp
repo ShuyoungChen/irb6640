bb346664c6717d000a253621574f92dd6c184512
Shuyang Chen <shuyangchen01@gmail.com>
2017-09-27 18:09:18 -0400

    add overhead cameras

--- path_planning/src/path_planning.cpp
+++ path_planning/src/path_planning.cpp
@@ -18,130 +18,221 @@
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
 geometry_msgs::Pose pose;
-
+bool empty;  // whether the wrist camera can track any ar tags, if not, use the left overhead camera
 
 // Callback function for vision servo
 void alvarCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {   
-    geometry_msgs::Pose p;
-    p = msg->markers[0].pose.pose;
-    tf::TransformListener listener;
-    tf::StampedTransform transform;
-    tf::Transform tr, new_pose;
-    
-    tf::Quaternion q_prim = tf::Quaternion(p.orientation.x,
-                            p.orientation.y,
-                            p.orientation.z,
-                            p.orientation.w) * tf::Quaternion(1, 0, 0, 0) * tf::Quaternion(0, 0, -0.707, 0.707);
+	if (msg->markers.empty()) {
+		empty = true;
+		ROS_INFO("The wrist camera cannot see the tag...");
+	}
+	else {
+		empty = false;
+
+		ROS_INFO("The wrist camera can see the tag..");
+		geometry_msgs::Pose p;
+		p = msg->markers[0].pose.pose;
+		tf::TransformListener listener;
+		tf::StampedTransform transform;
+		tf::Transform tr, new_pose;
+
+		tf::Quaternion q_prim = tf::Quaternion(p.orientation.x,
+				p.orientation.y,
+				p.orientation.z,
+				p.orientation.w) * tf::Quaternion(1, 0, 0, 0) * tf::Quaternion(0, 0, -0.707, 0.707);
 
 
-    tr.setRotation(tf::Quaternion(q_prim.x(),
-                            q_prim.y(),
-                            q_prim.z(),
-                            q_prim.w()));
-    tr.setOrigin(tf::Vector3(p.position.x, p.position.y, p.position.z-0.6));
-   
-    ros::Time now = ros::Time(0);
-    listener.waitForTransform("/base_link", "/camera_link_optical", now, ros::Duration(3.0));
-    listener.lookupTransform("/base_link", "/camera_link_optical", now, transform);
-  
-    new_pose = transform * tr;
+		tr.setRotation(tf::Quaternion(q_prim.x(),
+					q_prim.y(),
+					q_prim.z(),
+					q_prim.w()));
+		tr.setOrigin(tf::Vector3(p.position.x, p.position.y, p.position.z-0.5));
 
-    // Construct a target frame for visualization in Rviz
-    tf::TransformBroadcaster br;
-    br.sendTransform(tf::StampedTransform(new_pose, now, "world", "target"));
+		ros::Time now = ros::Time(0);
+		listener.waitForTransform("/base_link", "/camera_link_optical", now, ros::Duration(3.0));
+		listener.lookupTransform("/base_link", "/camera_link_optical", now, transform);
 
-    pose.position.x = new_pose.getOrigin().x();
-    pose.position.y = new_pose.getOrigin().y();
-    pose.position.z = new_pose.getOrigin().z();
-    pose.orientation.x = new_pose.getRotation().x();
-    pose.orientation.y = new_pose.getRotation().y();
-    pose.orientation.z = new_pose.getRotation().z();
-    pose.orientation.w = new_pose.getRotation().w();  
+		new_pose = transform * tr;
+
+		// Construct a target frame for visualization in Rviz
+		tf::TransformBroadcaster br;
+		br.sendTransform(tf::StampedTransform(new_pose, now, "world", "target"));
+
+		pose.position.x = new_pose.getOrigin().x();
+		pose.position.y = new_pose.getOrigin().y();
+		pose.position.z = new_pose.getOrigin().z();
+		pose.orientation.x = new_pose.getRotation().x();
+		pose.orientation.y = new_pose.getRotation().y();
+		pose.orientation.z = new_pose.getRotation().z();
+		pose.orientation.w = new_pose.getRotation().w();  
+	}
 }
+
+
+/******* Convert roll, pith, yaw and quaternions. *******/
+
+/*tf::Quaternion q(req.markers[0].pose.pose.orientation.x, 
+  req.markers[0].pose.pose.orientation.y,
+  req.markers[0].pose.pose.orientation.z,
+  req.markers[0].pose.pose.orientation.w);
+
+  tf::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+ */
+
+
+void alvarCallback_2(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {   
+	if (empty) {
+		ROS_INFO("The wrist camera cannot see the tag, use overhead camera instead...");
+		if (!msg->markers.empty()) {
+			geometry_msgs::Pose p;
+
+			p = msg->markers[0].pose.pose;
+
+			//p = msg->markers[0].pose.pose;
+			tf::TransformListener listener;
+			tf::StampedTransform transform;
+			tf::Transform tr, new_pose;
+
+			tf::Quaternion q_prim = tf::Quaternion(p.orientation.x,
+					p.orientation.y,
+					p.orientation.z,
+					p.orientation.w) * tf::Quaternion(1, 0, 0, 0) * tf::Quaternion(0, 0, -0.707, 0.707);
+
+
+			tr.setRotation(tf::Quaternion(q_prim.x(),
+						q_prim.y(),
+						q_prim.z(),
+						q_prim.w()));
+			tr.setOrigin(tf::Vector3(p.position.x, p.position.y, p.position.z-0.6));
+
+			ros::Time now = ros::Time(0);
+			listener.waitForTransform("/base_link", "/world_camera_link_optical_2", now, ros::Duration(3.0));
+			listener.lookupTransform("/base_link", "/world_camera_link_optical_2", now, transform);
+
+			new_pose = transform * tr;
+
+			// Construct a target frame for visualization in Rviz
+			tf::TransformBroadcaster br;
+			br.sendTransform(tf::StampedTransform(new_pose, now, "world", "target"));
+
+			pose.position.x = new_pose.getOrigin().x();
+			pose.position.y = new_pose.getOrigin().y();
+			pose.position.z = new_pose.getOrigin().z();
+
+			//pose.orientation.x = new_pose.getRotation().x();
+			//pose.orientation.y = new_pose.getRotation().y();
+			//pose.orientation.z = new_pose.getRotation().z();
+			//pose.orientation.w = new_pose.getRotation().w(); 
+
+			pose.orientation.x = 0.0;
+			pose.orientation.y = 1.0;
+			pose.orientation.z = 0.0;
+			pose.orientation.w = 0.0; 
+		}
+	}
+}
+
 
 // Callback function for force control
 //void forceCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "path_planning");
-  ros::NodeHandle node_handle;
-  
-  // subscriber for force, not used yet
-  /*ros::Subscriber sub1 = node_handle.subscribe<ar_track_alvar_msgs::AlvarMarkers>("/ar_pose_marker", 100, &alvarCallback); 
-  
-  while (ros::ok()) {
-    ros::spinOnce();
-  }
-  */
+	ros::init(argc, argv, "path_planning");
+	ros::NodeHandle node_handle;
 
-  // subscriber for vision topic
-  ros::CallbackQueue vision_queue;
-  ros::SubscribeOptions opt = ros::SubscribeOptions::create<ar_track_alvar_msgs::AlvarMarkers>(
-      "/ar_pose_marker",
-      100,
-      &alvarCallback,
-      ros::VoidPtr(),
-      &vision_queue);
-  ros::Subscriber vision_sub = node_handle.subscribe(opt); 
-  
-  // Deal with thread block
-  ros::AsyncSpinner spinner(1, &vision_queue);
-  spinner.start();
-  
-  ros::AsyncSpinner mainspinner(1);
-  mainspinner.start();
+	// subscriber for force, not used yet
+	/*ros::Subscriber sub1 = node_handle.subscribe<ar_track_alvar_msgs::AlvarMarkers>("/ar_pose_marker", 100, &alvarCallback); 
 
-  static const std::string PLANNING_GROUP = "manipulator";
-  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+	  while (ros::ok()) {
+	  ros::spinOnce();
+	  }
+	 */
 
-  // Raw pointers are frequently used to refer to the planning group for improved performance.
-  //const robot_state::JointModelGroup *joint_model_group =
-    //move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
- 
-  namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
-  visual_tools.deleteAllMarkers();
+	// subscriber for vision topic
+	ros::CallbackQueue vision_queue;
+	ros::SubscribeOptions opt = ros::SubscribeOptions::create<ar_track_alvar_msgs::AlvarMarkers>(
+			"/ar_pose_marker_1",
+			100,
+			&alvarCallback,
+			ros::VoidPtr(),
+			&vision_queue);
+	ros::Subscriber vision_sub = node_handle.subscribe(opt); 
 
-  visual_tools.loadRemoteControl();
+	// Deal with thread block
+	ros::AsyncSpinner spinner(1, &vision_queue);
+	spinner.start();
 
-  // Rviz provides many types of markers, in this demo we will use text, cylinders, and spheres
-  /*Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
-  text_pose.translation().z() = 1.75; // above head of PR2
-  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
-  */
-  // Batch publishing is used to reduce the number of messages being sent to Rviz for large visualizations
-  visual_tools.trigger();
-  
-  ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
-  moveit_msgs::DisplayTrajectory display_trajectory;
+	// subscribe to the left overhead camera topic if wrist camera cannot see the tag
 
-  ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group.getPlanningFrame().c_str());
-  ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+	ros::CallbackQueue vision_queue_2;
+	ros::SubscribeOptions opt_2 = ros::SubscribeOptions::create<ar_track_alvar_msgs::AlvarMarkers>(
+			"/ar_pose_marker_2",
+			100,
+			&alvarCallback_2,
+			ros::VoidPtr(),
+			&vision_queue_2);
+	ros::Subscriber vision_sub_2 = node_handle.subscribe(opt_2); 
 
-  move_group.setGoalPositionTolerance(0.004);
-  move_group.setGoalOrientationTolerance(0.01);
-  move_group.allowReplanning(true);
+	ros::AsyncSpinner spinner_2(1, &vision_queue_2);
+	spinner_2.start();
 
-  while (ros::ok()) {
-      move_group.setPoseTarget(pose);  
-      moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-      bool success = move_group.plan(my_plan);
-      ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-      if (success) {
-          move_group.execute(my_plan);
-          std::cout << "executed" << std::endl;
-      }
-      else
-          ROS_ERROR("Cannot find a plan.");
+	ros::AsyncSpinner mainspinner(1);
+	mainspinner.start();
 
-      ROS_INFO_NAMED("tutorial", "Visualizing plan 1 as trajectory line");
-      visual_tools.publishAxisLabeled(pose, "pose1");
+	static const std::string PLANNING_GROUP = "manipulator";
+	moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-      ros::Duration(0.5).sleep();
-  }
+	// Raw pointers are frequently used to refer to the planning group for improved performance.
+	//const robot_state::JointModelGroup *joint_model_group =
+	//move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-  ros::waitForShutdown();
-  return 0;
+	namespace rvt = rviz_visual_tools;
+	moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
+	visual_tools.deleteAllMarkers();
+
+	visual_tools.loadRemoteControl();
+
+	// Rviz provides many types of markers, in this demo we will use text, cylinders, and spheres
+	/*Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
+	  text_pose.translation().z() = 1.75; // above head of PR2
+	  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+	 */
+	// Batch publishing is used to reduce the number of messages being sent to Rviz for large visualizations
+	visual_tools.trigger();
+
+	ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+	moveit_msgs::DisplayTrajectory display_trajectory;
+
+	ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group.getPlanningFrame().c_str());
+	ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+
+	move_group.setGoalPositionTolerance(0.004);
+	move_group.setGoalOrientationTolerance(0.01);
+	move_group.allowReplanning(true);
+
+	while (ros::ok()) {
+		move_group.setPoseTarget(pose);  
+		moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+		bool success = move_group.plan(my_plan);
+		ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+		if (success) {
+			move_group.execute(my_plan);
+			std::cout << "executed" << std::endl;
+		}
+		else
+			ROS_ERROR("Cannot find a plan.");
+
+		ROS_INFO_NAMED("tutorial", "Visualizing plan 1 as trajectory line");
+		visual_tools.publishAxisLabeled(pose, "pose1");
+
+		ros::Duration(0.5).sleep();
+	}
+
+	ros::waitForShutdown();
+	return 0;
 }
+
