9e034020df59e64a1802da32a983de61af734114
Shuyang Chen <shuyangchen01@gmail.com>
2017-11-07 15:17:31 -0500

    code cleanup

--- irb6640_moveit_config/src/vacuum_gripper_ctrl.cpp
+++ irb6640_moveit_config/src/vacuum_gripper_ctrl.cpp
@@ -5,32 +5,39 @@
 class Vacuum_gripper {
 
 	public:
-		void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {   
-			if (joy->axes[4] == 1) {
-				client.call(srv);
-				std::cout << "turning on vacuum gripper..." << std::endl;
-			}
-			else if (joy->axes[4] == -1) {
-				client_off.call(srv);
-				std::cout << "turning off vacuum gripper..." << std::endl;
-			}
-		}
+		Vacuum_gripper();
 
+	private:
+		void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
-	protected:
-		ros::NodeHandle n_;
-		std_srvs::Empty srv;
-		ros::Subscriber sub; 
-		ros::ServiceClient client = n_.serviceClient<std_srvs::Empty>("irb6640/vacuum_gripper/on");
-		ros::ServiceClient client_off = n_.serviceClient<std_srvs::Empty>("irb6640/vacuum_gripper/off");
+		ros::NodeHandle nh_;
+
+		std_srvs::Empty srv_;
+		ros::Subscriber sub_; 
+		ros::ServiceClient client_on_, client_off_;
 };
+
+Vacuum_gripper::Vacuum_gripper() {
+	sub_ = nh_.subscribe<sensor_msgs::Joy>("/joy", 100, &Vacuum_gripper::joyCallback, this);
+	client_on_ = nh_.serviceClient<std_srvs::Empty>("irb6640/vacuum_gripper/on");
+	client_off_ = nh_.serviceClient<std_srvs::Empty>("irb6640/vacuum_gripper/off");
+};
+
+void Vacuum_gripper::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {   
+	if (joy->axes[4] == 1) {
+		client_on_.call(srv_);
+		std::cout << "turning on vacuum gripper..." << std::endl;
+	}
+	else if (joy->axes[4] == -1) {
+		client_off_.call(srv_);
+		std::cout << "turning off vacuum gripper..." << std::endl;
+	}
+}
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "vacuum_gripper_ctrl");
-	ros::NodeHandle nh;
-	ros::Subscriber sub;
 
 	Vacuum_gripper v;
-	sub = nh.subscribe<sensor_msgs::Joy>("/joy", 1000, &Vacuum_gripper::joyCallback, &v);
 	ros::spin();
 }
+
